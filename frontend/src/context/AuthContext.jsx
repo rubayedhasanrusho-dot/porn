0cca906f5f62ef06469248ce55980963b530eedf
rubayedhasanrusho-dot <rubayedhasanrusho@gmail.com>
2026-06-25 03:52:15 +0600

    connect frontend to render backend

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
+axios.defaults.baseURL = 'https://porn-3vv9.onrender.com';
 
 const AuthContext = createContext(null);
 
